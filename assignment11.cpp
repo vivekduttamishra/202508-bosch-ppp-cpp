c76b64629652353ad355ff2ecfe32fd3764aa717
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2025-08-18 15:51:52 +0530

    day#1 update#2

---
+++ assignment11.cpp
@@ -0,0 +1,24 @@
+//Create a Calculator that can perform arithmetic operations like one below
+
+
+
+int  main(){
+
+	Calculator calc;
+	
+	calc. CalculateResult( 2, "plus" , 3);
+	
+	calc. CalculateResult( 4, "minus", 1);
+
+	calc.CalculateResult(5, "foo", 20);
+}
+
+/* expected output
+
+2 plus 3 = 5
+4 minus 1 = 3
+Invalid operator : "foo"
+
+*/
+
+//Give your best code.
